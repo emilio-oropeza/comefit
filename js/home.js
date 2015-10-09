0793dea67f8c4b59ff4df8d4e6a16cc4e77f0976
vocho <vocho@underdog.gs>
2015-10-08 23:47:08 -0500

    menus
    menus colocados

---
+++ js/home.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$(".platillos").each(function(){
+		var platillos_desc = $(this).find(".platillos_desc");
+		$(this).on( "mouseenter", function() {
+    		$(platillos_desc).fadeIn("fast");
+  		})
+  		.on( "mouseleave", function() {
+   			$(platillos_desc).fadeOut("slow");
+ 		});
+	});
+	$("#menu_display").click(function(){
+		$("#menu_content").toggle();
+	});
+	$(".menu_element").each(function(){
+		$(this).click(function(){
+			if($(window).width() <= 800){
+				$("#menu_content").fadeOut("slow");
+			}
+		});
+	});
+	$(window).resize(function(){
+		if($(window).width() <= 800){
+			$("#menu_content").hide();
+		}else{
+			$("#menu_content").show();
+		}
+	});
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 2000);
+	    return false;
+	});
+});
